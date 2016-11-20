f1d95bf5245fb5aa96676bb1be1bf309000749ff
Denis Defreyne <denis.defreyne@stoneship.org>
2016-11-20 20:37:05 +0100

    Bundle Git deployer

--- lib/nanoc/deploying/deployers.rb
+++ lib/nanoc/deploying/deployers.rb
@@ -2,9 +2,11 @@ module Nanoc::Deploying
   # @api private
   module Deployers
     autoload 'Fog',   'nanoc/deploying/deployers/fog'
+    autoload 'Git',   'nanoc/deploying/deployers/git'
     autoload 'Rsync', 'nanoc/deploying/deployers/rsync'
 
     Nanoc::Deploying::Deployer.register '::Nanoc::Deploying::Deployers::Fog',   :fog
+    Nanoc::Deploying::Deployer.register '::Nanoc::Deploying::Deployers::Git',   :git
     Nanoc::Deploying::Deployer.register '::Nanoc::Deploying::Deployers::Rsync', :rsync
   end
 end
