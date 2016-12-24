aa8f79fc1de29d2b944bb4fe25835af1bee8d68a
Denis Defreyne <denis.defreyne@stoneship.org>
2016-12-24 12:05:04 +0100

    Allow calling #write twice

--- lib/nanoc/rule_dsl/rule_memory_calculator.rb
+++ lib/nanoc/rule_dsl/rule_memory_calculator.rb
@@ -85,7 +85,10 @@ module Nanoc::RuleDSL
         executor.snapshot(:last)
       end
 
-      assign_paths_to_mem(rule_memory, rep: rep)
+      # FIXME: convert to side-effect free transformation
+      mem = assign_paths_to_mem(rule_memory, rep: rep)
+      mem.canonicalize
+      mem
     end
 
     # @param [Nanoc::Int::Layout] layout
